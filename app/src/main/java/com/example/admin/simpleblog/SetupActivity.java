30e99e4ddf6d9facbf3deee7ef0509b8bd1a0c57
teodoratanasov <t.atanasov1516@gmail.com>
2017-11-08 16:22:57 +0200

    commit

--- app/src/main/java/com/example/admin/simpleblog/SetupActivity.java
+++ app/src/main/java/com/example/admin/simpleblog/SetupActivity.java
@@ -1,7 +1,9 @@
 package com.example.admin.simpleblog;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -9,12 +11,39 @@ import android.widget.ImageButton;
 public class SetupActivity extends AppCompatActivity {
 
     private ImageButton mSetupImageBtn;
-    private EditText mSetupField;
+    private EditText mNameField;
     private Button mSubmitBtn;
+
+    private static final int GALLERY_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        mSetupImageBtn = (ImageButton) findViewById(R.id.SetupImageBtn);
+        mNameField = (EditText) findViewById(R.id.SetupNameField);
+        mSubmitBtn = (Button) findViewById(R.id.SetupSubmitBtn);
+
+        mSetupImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+
+
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK);
     }
 }
